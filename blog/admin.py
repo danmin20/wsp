94903b420fc62fb3225ce1fa1a855a55b71edcb4
lee jeongmin <danmin20@khu.ac.kr>
2021-06-15 17:03:25 +0900

    실습과제

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
-from .models import Post
-
-admin.site.register(Post)
-
+from . import models
 # Register your models here.
+
+@admin.register(models.Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'published_date',
+        'created_date',
+        'author'
+    )
+    list_display_links = (
+        'title',
+        'author'
+    )
